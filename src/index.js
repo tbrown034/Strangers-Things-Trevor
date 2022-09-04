44079c6287a76c72437f9afe23c76137a4918369
tbrown034 <tbrown034@gmail.com>
2022-09-03 22:14:58 -0500

    was in a loop, now its not

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { useEffect, useState } from 'react';
 const BASE_URL = 'https://strangers-things.herokuapp.com/api/2208-ftb-et-web-ft';
 
-
 const App = () => {
 	const [allPosts, setAllPosts] = useState([]);
 	useEffect(() => {
@@ -18,14 +17,6 @@ const App = () => {
 		const allPostsList = result.results;
 		setAllPosts(allPostsList)
 	}
-
-	// const renderPosts = () => {
-	// for (let i = 0; i < posts.length; i++) {
-	// 	const newPostDiv = document.getElementById()
-	// 	const postElement.append
-	// 	const displayPost = posts[i]; }
-	// }
-
 	return (
 		<div>
 			<nav>
@@ -36,56 +27,11 @@ const App = () => {
 			</nav>
 			<div>
 				<h2>All forms</h2>
-					<ul>
-						{
-							allPosts.map (() => {
-								return 
-							}
-							)
-						}
-					</ul>
-			</div>
-			<div>
-				<h2>Hello Everyone</h2>
+
 			</div>
 		</div>
-	)
-}
-
-///////////// Posts w/ API //////////
-
-// const postElement = document.getElementById("posts")
-
-/// Getting initial posts
-// const fetchPosts = async () => {    
-// 		const response = await fetch (`${BASE_URL}/posts`);
-//         const result = await response.json()
-//         console.log(result)
-// 		renderPosts()
-// 	}
-
-// const renderPosts = () => {
-// 	for (let i = 0; i < posts.length; i++) {
-// 		const postElement.append
-// 		const displayPost = posts[i];
-
-
-		
-
-// 	}
-// }
-
-
-
+	)}
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<App/>);
-
-///bootstrapping
-fetchPosts()
-
-
-
-
-
 
