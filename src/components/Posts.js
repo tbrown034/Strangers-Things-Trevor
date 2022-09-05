5bd5ce0f638feae886566d72f0ad058ac4dc59fa
tbrown034 <tbrown034@gmail.com>
2022-09-05 16:33:29 -0500

    quick fix fixed child issue

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,51 +3,43 @@ import { useState, useEffect } from "react";
 import { BASE_URL } from '../api'
 /// Not to self: allPosts is state.allPosts and setAllPosts is a function that sets state.allPosts ... and state is an object with keys and values. State is an object that holds our data (that will change)
 const Posts = () => {
-    const [allPosts, setAllPosts] = useState([]);
-    useEffect(() => {
-        const fetchPosts = async () => {    
-            const response = await fetch (`${BASE_URL}/posts`);
-            const allPostObj = await response.json();
-            const allPostsList = allPostObj.data.posts;
-            setAllPosts(allPostsList);
-        }
-        fetchPosts()
+	const [allPosts, setAllPosts] = useState([]);
+	useEffect(() => {
+		const fetchPosts = async () => {    
+			const response = await fetch (`${BASE_URL}/posts`);
+			const allPostObj = await response.json();
+			const allPostsList = allPostObj.data.posts;
+			setAllPosts(allPostsList);
+		}
+		fetchPosts()
 }, []);
 
 return (
    <div>
-        <div>
-            <h2 id="posts"> All Posts</h2>
-
-            
-		        {
-                allPosts.map((singlePost, i) => {
-			    return (
-                    <div id="marketPlace">
-                        <ul>
-                        <li key={i}> {singlePost.title}</li>
-                            <ul>
-                                <li key={i}> Price: {singlePost.price}</li>
-                                <li key={i}> description: {singlePost.price}</li>
-                                <li key={i}> Seller: {singlePost.author.username}</li>
-                                <li key={i}> Location: {singlePost.location}</li>
-                                <li key={i}> Post Created: {singlePost.createdAt}</li>
-
-
-
-                            </ul>
-                        </ul>
-                    </div>
-                ) 
-                
-                
-			    } )
-		        }
-        </div>	
+		<div>
+			<h2 id="posts"> All Posts</h2>
+				{
+				allPosts.map((singlePost, i) => {
+				return (
+					<div id="marketPlace">
+						<ul>
+						<li key={i}> {singlePost.title}</li>
+							<ul>
+								<li> Price: {singlePost.price}</li>
+								<li> description: {singlePost.price}</li>
+								<li> Seller: {singlePost.author.username}</li>
+								<li> Location: {singlePost.location}</li>
+								<li> Post Created: {singlePost.createdAt}</li>
+							</ul>
+						</ul>
+					</div>
+				) 
+				} )
+				}
+		</div>	
 	</div> 
-        )
+		)
 }
-
 export default Posts 
 
 
