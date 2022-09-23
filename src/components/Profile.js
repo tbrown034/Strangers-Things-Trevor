260362731482f384198f2961c3f2b0f4f6fce492
tbrown034 <tbrown034@gmail.com>
2022-09-23 02:21:07 -0500

    broken but I'm about to change more

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,33 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getUserInfo } from "../api";
 
 const Profile = () => {
   const [myInfo, setMyInfo] = useState({});
   let token = localStorage.getItem("token");
-  if (token) {
-      const getMyInfo = () => {
-      const returnedInfo =  getUserInfo(token);
-      setMyInfo(returnedInfo);
-      }
-    ;
+  if (!token) {
+    return
+  }
+    else {useEffect(() => {
+          const getMyInfo = () => {
+          const returnedInfo =  getUserInfo(token);
+          setMyInfo(returnedInfo);
+          }
+        getMyInfo();
+    }, []);}
+
   return (
     <div>
-      <h1>Welcome to your profile you {myInfo.username} !</h1>
+      <h1>Welcome to your profile  {myInfo.data.username.value} !</h1>
       <div>Your Messages:</div>
       <div>You currently have no messages!</div>
     </div>
   )
-};}
+
+
+};
 
 export default Profile;
 
-// useEffect(() => {
-//   let token = localStorage.getItem("token");
-//   if (token) {
-//       const getMyInfo = () => {
-//       const returnedInfo =  getUserInfo(token);
-//       setMyInfo(returnedInfo);
-//       }
-//     getMyInfo();
-//   }
-// }, []);
